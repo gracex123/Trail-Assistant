11f23141a7a1e17fc8776113b45c8683000047a1
gracex123 <yxiao29@jhu.edu>
2023-08-09 22:42:22 -0700

    api for recommendation engine added

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ import pkg from "@prisma/client";
 import morgan from "morgan";
 import cors from "cors";
 import { auth } from  'express-oauth2-jwt-bearer'
+import { Parser } from 'json2csv';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 const requireAuth = auth({
   audience: process.env.AUTH0_AUDIENCE,
@@ -19,13 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Enable CORS middleware
-//app.use((req, res, next) => {
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  //res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  //next();
-//});
 
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
@@ -34,30 +30,213 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+async function convertToCSV (res, fileName, fields, data) {
+  try{
+    const json2csv = new Parser({ fields });
+    const csv = json2csv.parse(data);
 
-//Get trail image link from external api
-app.get("/images/:title", async (req, res) => {
+    // Save the CSV file to the server or perform any other desired operations
+    res.header('Content-Type', 'text/csv');
+    res.attachment(fileName);
+    return res.send(csv);
+  } catch (err) {
+    // Handle any errors that might occur during CSV conversion or sending the response
+    console.error('Error converting to CSV:', err);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
+app.get("/trail-comments/:amendedTrailTitle", async (req, res) => {
   try {
-    const trailTitle = req.params.title;
-    if (!trailTitle) {
-      res.status(401).send('incorrect input values')
-    }
+    const amendedTrailTitle = req.params.amendedTrailTitle;
+    const url = `https://www.vancouvertrails.com/trails/${amendedTrailTitle}/comments/`;
 
-    const response = await fetch(`https://api.bing.microsoft.com/v7.0/images/search?q=${trailTitle}`, {
-        method: 'GET',
-        headers: {
-          'Ocp-Apim-Subscription-Key': '8e6202051d0749fea9929b551b6d0a84',
-        },
-    });
-    const data = await response.json();
-    const url = data.value[0].webSearchUrl;
-    res.json({ url });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    const response = await axios.get(url);
+
+    if (!response.ok) throw new Error('Error scraping the trail website.');
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const ratingContainer = $('.overall_rating span');
+
+    if (!ratingContainer.ok) throw new Error('Rating information not found.');
+
+    const ratingInfo = ratingContainer.text().trim();
+    const ratingInfoArray = ratingInfo.split(' ');
+    const rating = ratingInfoArray[0];
+    const totalReviews = ratingInfoArray[ratingInfoArray.length - 1];
+
+    const result = {
+      rating: rating.toString(),
+      reviewCount: totalReviews,
+    };
+    res.json(result);
+
+  } catch (err) {
+    console.log(`${err}`);
+    res.status(500).json({ error: 'Unable to fetch trail data.' });
   }
 });
 
+
+
+async function scrapeRatings(trailTitle) {
+  try {
+    let amendedTitles = "";
+    if (trailTitle === "Al's Habrich Ridge Trail") {
+      amendedTitles = "als-habrich-ridge-trail";
+    } else {
+      const title = trailTitle.toLowerCase().replace(/\s/g, "-")
+      const titleWithoutQuote = title.replace(/'/g, "");
+      amendedTitles = titleWithoutQuote;
+    }      
+    const url = `https://www.vancouvertrails.com/trails/${amendedTitles}/comments/`;
+
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const ratingContainer = $('.overall_rating span');
+
+    if (ratingContainer) {
+      const ratingInfo = ratingContainer.text().trim();
+      const ratingInfoArray = ratingInfo.split(' ');
+      const rating = ratingInfoArray[0];
+      const result = rating.toString();
+      return result;
+    } else {
+      console.log('Rating information not found.');
+      throw new Error('Rating information not found.');
+    } 
+  } catch (err) {
+    console.log(`An error occurred while fetching trail data: ${err}`);
+    throw new Error('Unable to fetch trail data.');
+  }
+}
+
+async function getTrail(wishItem) {
+  try {
+    const trail = await prisma.trailItem.findUnique({
+      where: {
+        id: wishItem.trailId,
+      },
+    })
+    return trail;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+async function extractWishTrailData (wishItemsList, fields) {
+  //Use Promise.all() to await all the asynchronous tasks inside the map() function.
+  const data = await Promise.all(wishItemsList.map(async (item) => {
+    try {
+      const trail = await getTrail(item);
+      
+      if (!trail) throw new Error ('Trail item not found.')
+      const rating = await scrapeRatings(trail.trailTitle);
+      
+      if (!rating) throw new Error ('Trail rating not scraped.')
+
+      const extractedData = {[fields[0]]: trail.id, 
+        [fields[1]]: trail.difficulty, 
+        [fields[2]]: trail.publicTransit,
+        [fields[3]]: trail.dogFriendly,
+        [fields[4]]: trail.camping,
+        [fields[5]]: rating,
+        [fields[6]]: trail.trailTitle,};
+      
+      console.log(extractedData);
+      return extractedData;
+
+    } catch (err) {
+      console.error( `Error in extracting trail data`);
+    }
+  }));
+  return data.filter(item => item !== null);
+}
+
+//get a csv file of a specific user's wishlist data
+app.get("/csv/:auth0id", async (req, res) => {
+  try {
+    //const auth0Id = req.auth.payload.sub;
+    const auth0id = "auth0|" + req.params.auth0id;
+    const user = await prisma.user.findUnique({
+      where: {
+        auth0Id: auth0id,
+      },
+    });
+    
+    if (!user) throw new Error ('User not found.')
+    const wishItemsList = await prisma.wishItem.findMany({
+      where: {
+        authorId: user.id,
+      },
+    });
+   
+    if (!wishItemsList) throw new Error ('Wish items not found.')
+    const fields = ["id", "difficulty", "publicTransit", "dogFriendly", "camping", "rating", "trailTitle"];
+    const data = await extractWishTrailData(wishItemsList, fields);
+    if (!data) throw new Error ('Error in extracting trail data.')
+
+    const csvFile = await convertToCSV(res, "wishlist.csv", fields, data);
+    return csvFile;
+    
+  } catch (err) {
+    console.error('Error retrieving fields:', err);
+    res.status(500).send('Error retrieving fields');
+  }
+})
+
+
+async function extractTrailData (trailList, fields) {
+  //Use Promise.all() to await all the asynchronous tasks inside the map() function.
+  const data = await Promise.all(trailList.map(async (trail) => {
+    try {
+      const rating = await scrapeRatings(trail.trailTitle);
+      
+      if (!rating) throw new Error ('Trail rating not scraped.')
+
+      const extractedData = {[fields[0]]: trail.id, 
+        [fields[1]]: trail.difficulty, 
+        [fields[2]]: trail.publicTransit,
+        [fields[3]]: trail.dogFriendly,
+        [fields[4]]: trail.camping,
+        [fields[5]]: rating,
+        [fields[6]]: trail.trailTitle,};
+      
+      console.log(extractedData);
+      return extractedData;
+
+    } catch (err) {
+      console.error( `Error in extracting trail data`);
+    }
+  }));
+  return data.filter(item => item !== null);
+}
+
+
+//get a csv file of all trails' data
+app.get("/csv_all", async (req, res) => {
+  try {
+
+    const trailList = await prisma.trailItem.findMany();
+    if (!trailList) throw new Error ('Trail items not found.')
+
+    const fields = ["id", "difficulty", "publicTransit", "dogFriendly", "camping", "rating", "trailTitle"];
+    const data = await extractTrailData(trailList, fields);
+    if (!data) throw new Error ('Error in extracting trail data.')
+
+    const csvFile = await convertToCSV(res, "traillist.csv", fields, data);
+    return csvFile;
+    
+  } catch (err) {
+    console.error('Error retrieving fields:', err);
+    res.status(500).send('Error retrieving fields');
+  }
+})
 
 //Get all trails
 app.get("/trails/all", async (req, res) => {
@@ -142,12 +321,10 @@ app.get("/users/all", async(req, res) => {
 })
 
 
-//Get all wishitems by th userId
+//Get all wishitems by the userId
 app.get("/wishitems", requireAuth, async (req, res) => {
   const auth0Id = req.auth.payload.sub;
-
-  console.log(auth0Id)
-
+  
   const user = await prisma.user.findUnique({
     where: {
       auth0Id,
@@ -161,6 +338,7 @@ app.get("/wishitems", requireAuth, async (req, res) => {
   });
 
   res.json(wishItems);
+
 });
 
 
@@ -169,7 +347,6 @@ app.put("/wishitems/:id", requireAuth, async (req, res) => {
   const auth0Id = req.auth.payload.sub;
   
   const id = req.params.id;
-  console.log(auth0Id)
   const user = await prisma.user.findUnique({
     where: {
       auth0Id,
